1c81cb2c7b9e386c8c7e37e51a80d7cbc852fc4e
Rafael Mazolini Fernandes <rafael.fernandes@cnpem.br>
2025-03-16 11:50:59 -0300

    Deletes all

--- hardware/include/wifi_manager.h
+++
@@ -1,9 +0,0 @@
-#ifndef WIFI_MANAGER_H
-#define WIFI_MANAGER_H
-
-#include <Arduino.h>
-#include <WiFiManager.h>
-
-void initWifiManager();
-
-#endif
--- hardware/src/sensor_data.cpp
+++
@@ -1,3 +0,0 @@
-#include "sensor_data.h"
-
-SensorData sensorData;
